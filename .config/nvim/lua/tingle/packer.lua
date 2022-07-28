30a986955c37b4123c2ee46fc7eb18b032035503
tingle <tingle@apple.com>
2022-07-28 01:36:55 -0500

    add treesitter

--- .config/nvim/lua/tingle/packer.lua
+++ .config/nvim/lua/tingle/packer.lua
@@ -9,4 +9,8 @@ return require('packer').startup(function(use)
   use 'folke/tokyonight.nvim'
   use 'nvim-lua/plenary.nvim'
   use {'nvim-telescope/telescope.nvim', tag = '0.1.0', branch = '0.1.x' }
+  use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
 end)
