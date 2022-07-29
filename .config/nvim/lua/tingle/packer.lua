3980251e9dcfc6f1d89e616dc0d70397198b4bac
tim <tim@pop-os.localdomain>
2022-07-29 04:08:40 -0500

    add Comment.nvim to plugins

--- .config/nvim/lua/tingle/packer.lua
+++ .config/nvim/lua/tingle/packer.lua
@@ -4,13 +4,19 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'folke/tokyonight.nvim'
-  use 'nvim-lua/plenary.nvim'
-  use {'nvim-telescope/telescope.nvim', tag = '0.1.0', branch = '0.1.x' }
-  use {
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use 'folke/tokyonight.nvim'
+    use 'nvim-lua/plenary.nvim'
+    use {'nvim-telescope/telescope.nvim', tag = '0.1.0', branch = '0.1.x' }
+    use {
         'nvim-treesitter/nvim-treesitter',
         run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
     }
 end)
